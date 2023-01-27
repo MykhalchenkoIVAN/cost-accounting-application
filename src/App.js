f7516b2fde2b33ff73661ff898c6f9c67bcb3cf7
Ivan <ivanogamer2019@gmail.com>
2023-01-27 18:29:27 +0100

    1.0.2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import Costs from "./components/Costs";
+import Costs from "./components/Costs/Costs";
+import NewCost from "./components/Costs/NewCost/NewCost"
 
-function App() {
+const App = () => {
   const costs = [
     {
+      id: 'c1',
       date: new Date(2023, 0, 25),
       description: "Холодильник",
       amount: 999.99
     }, {
+      id: 'c2',
       date: new Date(2023, 0, 30),
       description: "MacBook",
       amount: 1400.99
     },
     {
+      id: 'c3',
       date: new Date(2023, 8, 25),
       description: "Холодильник",
       amount: 49.99
     }
   ]
 
+  // getting data from NewCost to the App.js component
+  const addCostHendler = (cost) => {
+    console.log("ssdds", cost);
+
+  }
 
   return (
     <div>
+      <NewCost onAddCost={addCostHendler} />
       <Costs costs={costs} />
     </div>
   );
